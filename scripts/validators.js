dd86891612d266ba1cf76f18d00047c100291014
skkohli <kohli.iitm@gmail.com>
2016-04-20 16:55:51 +0530

    Added custom external validators functions

---
+++ scripts/validators.js
@@ -0,0 +1,10 @@
+function ay1(value) {
+            //validation code
+            console.log("validate1");
+            return value.length > 4;
+        }
+function ay2(value) {
+            //validation code
+            console.log("validate2");
+            return value.length > 10;
+        }
